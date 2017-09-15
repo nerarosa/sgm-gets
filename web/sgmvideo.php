06b3b888fe4d4f589399b03694b5032424f1dba0
nerarosa <tienhv0502@gmail.com>
2017-09-15 14:25:20 +0700

    a

--- web/sgmvideo.php
+++ web/sgmvideo.php
@@ -3,10 +3,11 @@ require "getvideos.php";
 
 header("Content-type: application/json; charset=utf-8");
 //header("Content-type: text/html; charset=utf-8");
-header('Access-Control-Allow-Origin: http://sexygirlmedia.blogspot.com');
-//header('Access-Control-Allow-Origin: http://www.sexygirlmedia.com');
+header('Access-Control-Allow-Origin: http://sexygirlmedia.blogspot.com', false);
+header('Access-Control-Allow-Origin: https://sexygirlmedia.blogspot.com', false);
 header("Access-Control-Allow-Credentials: true ");
 header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
 
 $videos = new getvideos();
 $videos->run();
+
